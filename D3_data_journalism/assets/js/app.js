69ea5bdd877f854aa006702314d124da2261b424
jskyler <72110869+jskyler@users.noreply.github.com>
2021-02-08 19:04:32 -0600

    Starting checkpoint

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,27 @@
 // @TODO: YOUR CODE HERE!
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+d3.csv("../assets/data/data.csv").then(function(newsData) {
+    console.log(newsData);
+
+
+});
