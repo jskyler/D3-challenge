1d2cab240a6279e9ed09d8f988833869b0822f1d
jskyler <72110869+jskyler@users.noreply.github.com>
2021-02-09 22:34:43 -0600

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -31,11 +31,11 @@ d3.csv("../assets/data/data.csv").then(function(newsData) {
     });
 
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(newsData, d => d.healthcare)])
+      .domain([8, d3.max(newsData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(newsData, d => d.poverty)])
+      .domain([4, d3.max(newsData, d => d.healthcare)])
       .range([height, 0]);
 
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -53,8 +53,8 @@ d3.csv("../assets/data/data.csv").then(function(newsData) {
       .data(newsData)
       .enter()
       .append("circle")
-      .attr("cx", d => xLinearScale(d.healthcare))
-      .attr("cy", d => yLinearScale(d.poverty))
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
       .attr("r", "15")
       .attr("fill", "steelblue")
       .attr("opacity", ".5");
@@ -63,13 +63,13 @@ d3.csv("../assets/data/data.csv").then(function(newsData) {
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (height / 1.5))
       .attr("dy", "1em") // NOTE 'em' is 10px units and 'd'  is like a 'delta' up the y-axis in this case
       .attr("class", "axisText")
       .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2.3}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("In Poverty (%)");
 
